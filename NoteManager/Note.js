1e5737e076c6550204c4f16203563c7956e212dc
Shivi Sharma <pranavsharma2126@gmail.com>
2020-08-19 19:02:53 +0530

    addnote

--- NoteManager/Note.js
+++ NoteManager/Note.js
@@ -1,7 +1,10 @@
 export default class Note {
-  constructor({ title, body }, notemanager) {
+  constructor({ title, body ,date,time,url }, notemanager) {
     this.title = title;
     this.body = body;
+    this.date = date;
+    this.time =time;
+    this.url =url;
     this.el = null;
     this.notemanager = notemanager;
   }
@@ -10,9 +13,14 @@ export default class Note {
     const tempdiv = document.createElement("div");
     tempdiv.innerHTML = tpl
       .replace("{{title}}", this.title)
-      .replace("{{body}}", this.body);
-    this.el = tempdiv.children[0];
+      .replace("{{body}}", this.body)
+      .replace("{{date}}",this.date)
+      .replace("{{time}}",this.time)
+      .replace("{{url}}",this.url)
+      .replace("{{url-1}}",this.url);
 
+    this.el = tempdiv.children[0];
+    
     this.eventlisteners();
 
     return this.el;
@@ -24,12 +32,19 @@ export default class Note {
                 <div class="mynote-close">
                     <i class="fas fa-trash"></i>
                 </div>
-                <div class="mynote-title" contenteditable>
+  
+                <div class="mynote-title" contenteditable >
                     {{title}}
                 </div>
             </div>
             <div class="mynote-body" contenteditable>
                 {{body}}
+            </div>
+            <div class="mynote-date" >
+                {{date}}  {{time}}
+            </div>
+            <div class="mynote-url" ><a href="{{url}}" target="_blank" class="urltag">
+                {{url-1}}</a>
             </div>
         </div>
         `;
@@ -42,11 +57,19 @@ export default class Note {
     };
 
     const title = this.el.querySelector(".mynote-title");
+    
     title.oninput = (ev) => {
       this.title = ev.target.innerHTML;
       this.notemanager.onnotechange(this);
     };
     const body = this.el.querySelector(".mynote-body");
+    body.onclick = () =>{
+      if(this.body === " (...click to add body...) "){
+      this.body = " ";
+      this.notemanager.renderNotes();
+      this.notemanager.onnotechange(this);
+      }
+    };
     body.oninput = (ev) => {
       this.body = ev.target.innerHTML;
       this.notemanager.onnotechange(this);
--- NoteManager/app.js
+++ NoteManager/app.js
@@ -1,15 +1,37 @@
 import NoteManager from "./notemanager.js";
 // import Note from "./Note";
+
+let date = "",
+  time = "";
+const getTime = async () => {
+  const response = await (
+    await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata")
+  ).json();
+  const res = response.datetime.split("T");
+  date = res[0];
+  time = res[1].split(".")[0];
+};
+
+
 chrome.storage.sync.get("notes", (data) => {
   const notemanager = new NoteManager({
     el: document.querySelector(".mynotes"),
     notes: data.notes,
   });
+  
   const newnotebtn = document.querySelector(".newnote");
   newnotebtn.onclick = () => {
     notemanager.addnote({
-      title: " ",
-      body: " ",
+      title: " Note added ",
+      body: " (...click to add body...) ",
+      time: time,
+      date: date,
+      url:'created by you'
+
     });
   };
+
+  notemanager.onnotechange=(noteobj)=>{
+    console.log(noteobj);
+};
 });
