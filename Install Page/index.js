12514e8b2c530dbc27e6ac82a320f3b679f9bd46
Pranav <pranavsharma2126@gmail.com>
2021-08-17 21:57:45 +0530

    logout and css added

--- Install Page/index.js
+++ Install Page/index.js
@@ -1,17 +1,30 @@
-import { signin} from '../storage.js';
+import { signin,getCurrentUser,logout} from '../storage.js';
 
 document.getElementById('login_button').addEventListener('click',async()=>{
   console.log("lol")
-  await signin()});
-document.getElementById('logout_button').addEventListener('click',logout);
+  await signin()
+  location.reload();
+});
+document.getElementById('logout_button').addEventListener('click',async()=>{
+  console.log("logout")
+  await logout()
+  location.reload();
+});
 
+window.onload = async()=>{
+  const user=await getCurrentUser();
+    if (user) {
+    console.log("signed in")
+    document.getElementById('login_button').style.display="none";
+    document.getElementById('logout_button').style.display="block";
 
-function logout() {
-  // firebase.auth().signOut().then((res)=>{
-  //   console.log("logged out");
-  // })
+    var uid = user.uid;
+  } else {
+    document.getElementById('login_button').style.display="block";
+    document.getElementById('logout_button').style.display="none";
+    console.log("signed out")
+  }
 }
-
 
 // firebase.auth().onAuthStateChanged((user) => {
 //   if (user) {
